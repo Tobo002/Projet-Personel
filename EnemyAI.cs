c4503c578d14d8c912de2bd7a46313a521c3506e
Tobo002 <95001731+Tobo002@users.noreply.github.com>
2022-01-04 22:09:18 -0500

    Add files via upload
    04/01

---
+++ EnemyAI.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float speed = 200f;
+    public float nextWaypointDistance = 3f;
+
+    public Transform GFX;
+
+    public Path path;
+    public int currentWaypoint = 0;
+    public bool reachedEndOfPath = false;
+
+    public Seeker seeker;
+    public Rigidbody2D rb;
+
+    private void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (path == null)
+            return;
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+        if (transform.position.x <= target.position.x)
+            GFX.rotation = new Quaternion(0f, 180f, 0f, 0f);
+        if (transform.position.x >= target.position.x)
+            GFX.rotation = new Quaternion(0f, 0f, 0f, 0f);
+    }
+}
---
+++ EnemyCombat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCombat : MonoBehaviour
+{
+
+    public float force = 500f;
+    public float radius = 5f;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 10)
+        {
+            Rigidbody2D player = collision.collider.GetComponent<Rigidbody2D>();
+            Rigidbody2D self = GetComponent<Rigidbody2D>();
+            Vector2 dir = player.position - self.position;
+            player.AddForce(dir.normalized * force);
+            self.AddForce(dir.normalized * -force);
+        }
+    }
+}
